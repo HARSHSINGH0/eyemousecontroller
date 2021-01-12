9b5d6814024b18ad5aa65d2524c861b4ce63c668
HARSHSINGH0 <hs475052@gmail.com>
2021-01-12 21:30:03 +0530

    some stuff added

--- eye_detection_p1.py
+++ eye_detection_p1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import dlib
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("testvideo.mp4")
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
