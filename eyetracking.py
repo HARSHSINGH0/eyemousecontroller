741d29123bbc0717816ff530dd8c01d3f6e8443f
HARSHSINGH0 <hs475052@gmail.com>
2021-01-14 17:43:34 +0530

    improvements

--- eyetracking.py
+++ eyetracking.py
@@ -2,13 +2,14 @@ import cv2 as cv
 cv2=cv
 import dlib
 
-cap=cv.VideoCapture(0)
+cap=cv.VideoCapture("testvideo.mp4")
 detector=dlib.get_frontal_face_detector()
 predictor=dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-def rescaleFrame(frame,scale=1):
-    width=int(frame.shape[1]*scale)#frame.shape[1] is width of image
-    height=int(frame.shape[0]*scale)#frame.shape[0] is height of image
-    dimension=(width,height)
+def rescaleFrame(frame):#scale=0.20
+    # width=int(frame.shape[1]*scale)#frame.shape[1] is width of image
+    # height=int(frame.shape[0]*scale)#frame.shape[0] is height of image
+    # dimension=(width,height)
+    dimension=(640,360)
     return cv.resize(frame,dimension,interpolation=cv.INTER_AREA)
 def midlinepoint(p1,p2):
     return int((p1.x+p2.x)/2),int((p1.y+p2.y)/2)
@@ -33,14 +34,16 @@ def eyetrack():
             down_point=(midlinepoint(landmarks.part(41),landmarks.part(40)))
             ver_line=cv.line(frame,up_point,down_point,(0,255,0),2)
 
-            left_point=(landmarks.part(42).x,landmarks.part(42).y)
-            right_point=(landmarks.part(45).x,landmarks.part(45).y)
-            hor_line=cv.line(frame,left_point,right_point,(0,255,0),2)
+            left_point_r=(landmarks.part(42).x,landmarks.part(42).y)
+            right_point_r=(landmarks.part(45).x,landmarks.part(45).y)
+            hor_line=cv.line(frame,left_point_r,right_point_r,(0,255,0),2)
             up_point=(midlinepoint(landmarks.part(43),landmarks.part(44)))
             down_point=(midlinepoint(landmarks.part(47),landmarks.part(46)))
             ver_line=cv.line(frame,up_point,down_point,(0,255,0),2)
 
-
+            if((up_point[1]-down_point[1])<1):
+                print("eye shut")
+            
         cv.imshow("frame",frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
--- mousecontrol_eye.py
+++ mousecontrol_eye.py
@@ -3,3 +3,11 @@ pyautogui.FAILSAFE = True
 from pynput.mouse import Listener,Button,Controller
 import time
 mouse= Controller()
+class mouse:
+
+    def left_click(a):
+        print("left click")
+    def right_click(a):
+        print("right click")
+
+    
