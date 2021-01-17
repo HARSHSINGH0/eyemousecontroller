8c30c0a4807d606a178d6679ef3e19fda6ac4170
HARSHSINGH0 <hs475052@gmail.com>
2021-01-17 11:13:17 +0530

    differentiating between intentional blinks and natural blinks

--- eyetracking.py
+++ eyetracking.py
@@ -33,19 +33,19 @@ def eyetrack(blinking_frames):
 
             facerect=cv2.rectangle(frame,(x,x1),(y,y1),(255,255,255),2)
             landmarks=predictor(gray,face)
-            left_point=(landmarks.part(36).x,landmarks.part(36).y)
-            right_point=(landmarks.part(39).x,landmarks.part(39).y)
-            hor_line=cv.line(frame,left_point,right_point,(255,255,255),2)
+            # left_point=(landmarks.part(36).x,landmarks.part(36).y)
+            # right_point=(landmarks.part(39).x,landmarks.part(39).y)
+            # hor_line=cv.line(frame,left_point,right_point,(255,255,255),2)
             up_point=(midlinepoint(landmarks.part(37),landmarks.part(38)))
             down_point=(midlinepoint(landmarks.part(41),landmarks.part(40)))
-            ver_line=cv.line(frame,up_point,down_point,(255,255,255),2)
+            ver_line=cv.line(frame,up_point,down_point,(255,255,255),1)
 
-            left_point_r=(landmarks.part(42).x,landmarks.part(42).y)
-            right_point_r=(landmarks.part(45).x,landmarks.part(45).y)
-            hor_line_r=cv.line(frame,left_point_r,right_point_r,(255,255,255),2)
+            # left_point_r=(landmarks.part(42).x,landmarks.part(42).y)
+            # right_point_r=(landmarks.part(45).x,landmarks.part(45).y)
+            # hor_line_r=cv.line(frame,left_point_r,right_point_r,(255,255,255),2)
             up_point_r=(midlinepoint(landmarks.part(43),landmarks.part(44)))
             down_point_r=(midlinepoint(landmarks.part(47),landmarks.part(46)))            
-            ver_line_r=cv.line(frame,up_point_r,down_point_r,(255,255,255),2)
+            ver_line_r=cv.line(frame,up_point_r,down_point_r,(255,255,255),1)
             value_of_blink=-3#this is for distance about 1 feet
 
             eyestonosepointx,eyestonosepointy=(landmarks.part(27).x,landmarks.part(27).y)
@@ -55,13 +55,19 @@ def eyetrack(blinking_frames):
             print("y1-x1 (for the area of the face):",y1-x1)
             print("distance of eye tips from up to down",(up_point[1]-down_point[1]))
             print(blinking_frames)
-            if((y1-x1)<105):
+            
+            if((y1-x1)>170):
+                value_of_blink=-6
+            elif((y1-x1)>140):
+                value_of_blink=-5
+            elif((y1-x1)>130):
+                value_of_blink=-5
+            elif((y1-x1)>120):
+                value_of_blink=-4
+            elif((y1-x1)<105):
                 cv.putText(frame,"come close to the camera",(80,150),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
                 value_of_blink=10
-            if((y1-x1)>170):
-                value_of_blink=-8
-            if((y1-x1)>130):
-                value_of_blink=-4
+            
             if((up_point[1]-down_point[1])>=value_of_blink):
                 blinking_frames+=1
                 if (blinking_frames>1):
@@ -71,7 +77,7 @@ def eyetrack(blinking_frames):
             else:
                 #print((up_point[1]-down_point[1]))
                 while blinking_frames!=0:
-                    blinking_frames=10
+                    blinking_frames=0
             #i could have done this with elif too but this will create priority to left clicks
 
             if((up_point_r[1]-down_point_r[1])>=value_of_blink):
