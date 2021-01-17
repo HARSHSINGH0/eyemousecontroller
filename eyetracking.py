d565942d58dfa2fa9dea44a8569ae543adade7b0
HARSHSINGH0 <hs475052@gmail.com>
2021-01-17 18:19:58 +0530

    improved blinks

--- eyetracking.py
+++ eyetracking.py
@@ -23,7 +23,7 @@ def eyetrack(blinking_frames):
         gray=rescaleFrame(gray)
         frame=rescaleFrame(frame)
         faces=detector(gray)
-        cv.putText(frame,"Q to exit",(250,50),cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+        cv.putText(frame,"Q to exit",(230,50),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,0),2)
         for face in faces:
             #print(face)
             x,y=face.left(),face.right()
@@ -33,25 +33,25 @@ def eyetrack(blinking_frames):
 
             facerect=cv2.rectangle(frame,(x,x1),(y,y1),(255,255,255),2)
             landmarks=predictor(gray,face)
-            # left_point=(landmarks.part(36).x,landmarks.part(36).y)
-            # right_point=(landmarks.part(39).x,landmarks.part(39).y)
-            # hor_line=cv.line(frame,left_point,right_point,(255,255,255),2)
+            left_point=(landmarks.part(36).x,landmarks.part(36).y)
+            right_point=(landmarks.part(39).x,landmarks.part(39).y)
+            hor_line=cv.line(frame,left_point,right_point,(255,255,255),2)
             up_point=(midlinepoint(landmarks.part(37),landmarks.part(38)))
             down_point=(midlinepoint(landmarks.part(41),landmarks.part(40)))
-            ver_line=cv.line(frame,up_point,down_point,(255,255,255),1)
+            ver_line=cv.line(frame,up_point,down_point,(255,255,255),2)
 
-            # left_point_r=(landmarks.part(42).x,landmarks.part(42).y)
-            # right_point_r=(landmarks.part(45).x,landmarks.part(45).y)
-            # hor_line_r=cv.line(frame,left_point_r,right_point_r,(255,255,255),2)
+            left_point_r=(landmarks.part(42).x,landmarks.part(42).y)
+            right_point_r=(landmarks.part(45).x,landmarks.part(45).y)
+            hor_line_r=cv.line(frame,left_point_r,right_point_r,(255,255,255),2)
             up_point_r=(midlinepoint(landmarks.part(43),landmarks.part(44)))
             down_point_r=(midlinepoint(landmarks.part(47),landmarks.part(46)))            
-            ver_line_r=cv.line(frame,up_point_r,down_point_r,(255,255,255),1)
+            ver_line_r=cv.line(frame,up_point_r,down_point_r,(255,255,255),2)
             value_of_blink=-3#this is for distance about 1 feet
 
             eyestonosepointx,eyestonosepointy=(landmarks.part(27).x,landmarks.part(27).y)
             #cv.rectangle(frame,(170,460),(320,202),(255,255,255),2)
-            # mouseclass.navigateto(eyestonosepointx,eyestonosepointy)
-            # print("blinking_frames",blinking_frames)
+            #mouseclass.navigateto(eyestonosepointx,eyestonosepointy)
+            print("blinking_frames",blinking_frames)
             print("y1-x1 (for the area of the face):",y1-x1)
             print("distance of eye tips from up to down",(up_point[1]-down_point[1]))
             print(blinking_frames)
@@ -71,7 +71,7 @@ def eyetrack(blinking_frames):
             if((up_point[1]-down_point[1])>=value_of_blink):
                 blinking_frames+=1
                 if (blinking_frames>1):
-                    cv.putText(frame,"Left click",(250,150),cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),3)
+                    cv.putText(frame,"Left click",(250,150),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
                     mouseclass.left_click()
                     break
             else:
@@ -84,7 +84,7 @@ def eyetrack(blinking_frames):
                 blinking_frames+=1
                 # print((up_point_r[1]-down_point_r[1]))
                 if (blinking_frames>1):
-                    cv.putText(frame,"Right click",(250,150),cv.FONT_HERSHEY_SIMPLEX,1,(255,255,255),3)
+                    cv.putText(frame,"Right click",(250,150),cv.FONT_HERSHEY_SIMPLEX,1,(0,0,0),3)
                     mouseclass.right_click()
                     break
             else:
