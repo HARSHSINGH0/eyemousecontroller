55e28856ab2839a9d413e044db78566e6880d7a0
HARSHSINGH0 <hs475052@gmail.com>
2021-03-13 09:43:01 +0530

    added all the updates

--- test.py
+++ test.py
@@ -1,25 +1,22 @@
-import cv2
-import numpy as np
-from imutils.video import WebcamVideoStream
-
-def adjust_gamma(image, gamma=1.0):
-
-   invGamma = 1.0 / gamma
-   table = np.array([((i / 255.0) ** invGamma) * 255
-      for i in np.arange(0, 256)]).astype("uint8")
-
-   return cv2.LUT(image, table)
-
-while True:
-   cap=WebcamVideoStream(src=0).start()
-   frame=cap.read()
-   original = frame
-   cv2.imshow('original',original)
-   gamma = 2                                   # change the value here to get different result
-   adjusted = adjust_gamma(original, gamma=gamma)
-   
-   cv2.imshow('gamma image 1',adjusted)
-   if cv2.waitKey(1) & 0xFF == ord('q') :
-       # self.cap.release()
-       cv2.destroyAllWindows()
-       break
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mylist=[]
+        longeststr=0
+        for i in s:
+            mylist.append(i)
+        for i in range(len(s)):
+            j=i+1
+            if j is len(s):
+                j=len(s)-1
+            print(mylist[i],mylist[j])
+            if mylist[i]!=mylist[j]:
+                longeststr+=1
+            else:
+                longeststr+=1
+        print(longeststr)
+a=Solution()
+a.lengthOfLongestSubstring("ab")
